a3345518f621e9a487d093e906ac0c3e3c62d305
Vadim Khaitov <polydora@rambler.ru>
2019-02-13 19:15:29 +0300

    вывесил ДЗ

--- Data_science/My_firs_steps/my_first_script.R
+++ Data_science/My_firs_steps/my_first_script.R
@@ -16,7 +16,7 @@ sqrt(9)
 sqrt(100/50*2) #используем функцию
 
 
-#Работа с переменными
+#Работа с переменными #####
 
 Gala <- (88 * 2 / 44)^2 
 
@@ -26,7 +26,7 @@ Tanya <- MarinA + Gala
 
 Renata <- (sqrt(Tanya))^Gala
 
-#Работа с векторами
+#Работа с векторами ######
 
 letters
 LETTERS
@@ -51,7 +51,7 @@ letters[5:10]
 
 seq(from = 5, to = 10, by = 2)
 
-#Пользовательские функции
+#Пользовательские функции#####
 
 uravn <- function(a, b) -b/a
 
@@ -80,11 +80,79 @@ sq_uravn(2, 0, -1)
 
 
 
-#Датафреймы
+#Датафреймы ##########
 
-install.packages("MASS")
+
+# install.packages("MASS")
 
 library(MASS)
+
+data(package = "MASS")
+
+data("birthwt")
+
+birthwt
+
+str(birthwt)
+
+?birthwt
+
+bwt <- birthwt
+
+bwt$smoke
+
+bwt$smoker [bwt$smoke == 1] <- "smoke" 
+bwt$smoker [bwt$smoke == 0] <- "none" 
+
+bwt$smoker <- ifelse(test = bwt$smoke == 1, yes = "smoke", no = "none")
+
+
+bwt$race2 <- ifelse(test = bwt$race == 1, yes = "white", no = ifelse(test = bwt$race == 2, yes = "black", no = "other"))
+
+bwt$race2[bwt$race == 1] <- "white"
+bwt$race2[bwt$race == 2] <- "black"
+bwt$race2[bwt$race == 3] <- "other"
+
+str(bwt)
+
+
+bwt$race2 <- factor(bwt$race2)
+str(bwt)
+
+bwt$smoker <- factor(bwt$smoker)
+str(bwt)
+
+
+## Визуализация данных #######
+
+## Базовая графика
+plot(x = bwt$age, y = bwt$bwt)
+
+
+## Пакет ggplot2
+library(ggplot2)
+
+ggplot(data = bwt, mapping = aes(x = age, y = bwt))
+
+ggplot(bwt, aes(x = age, y = bwt, color = smoker)) + 
+  geom_point(aes(size = lwt, shape = race2)) + 
+  labs(x = "Возраст матери", y = "Вес младенца", color = "Курение", size = "Вес матери") + 
+  theme_bw() + 
+  scale_color_manual(values = c("red", "blue")) +
+  theme(legend.position =  "right") +
+  facet_wrap( ~ race2)
+
+
+
+ggplot(bwt, aes(x = age, y = bwt, shape = smoker)) + 
+  geom_point(aes(color = lwt), size = 4) + 
+  labs(x = "Возраст матери", y = "Вес младенца", color = "вес матери", size = "Вес матери") + 
+  theme_bw() + 
+  theme(legend.position =  "right") +
+  facet_wrap( ~ race2)
+
+
+
 
 
 
